3f470893fa01813061400fd349f3f0277559d445
Neerav <144427415+blank360@users.noreply.github.com>
2025-12-10 02:49:12 +0530

    Delete Screens directory

--- Screens/Interview.js
+++
@@ -1,9 +0,0 @@
-import { Text ,View} from "react-native";
-
-export default function Interview({}){
-    return(
-        <View style={{flex:1,justifyContent:"center",alignContent:"center"}}>
-            <Text>Welcome to Interview Screen !!</Text>
-        </View>
-    )
-}
