f76f53ffb708e06807e6f3e67d0bdb57efef9819
Neerav <144427415+blank360@users.noreply.github.com>
2025-12-10 02:50:03 +0530

    Add files via upload

---
+++ Screens/Interview.js
@@ -0,0 +1,9 @@
+import { Text ,View} from "react-native";
+
+export default function Interview({}){
+    return(
+        <View style={{flex:1,justifyContent:"center",alignContent:"center"}}>
+            <Text>Welcome to Interview Screen !!</Text>
+        </View>
+    )
+}
