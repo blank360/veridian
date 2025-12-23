80e355949a7cc3cc1e7984a91a97e997b835056e
Neerav <144427415+blank360@users.noreply.github.com>
2025-12-23 09:53:33 +0530

    Change text color in CVMaker component
    Updated text color of CV Maker module.

--- Screens/CVMaker.js
+++ Screens/CVMaker.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 export default function CVMaker() {
-  return <View style={styles.center}><Text>CV Maker Module</Text></View>;
+  return <View style={styles.center} ><Text style={{ color: '#22c55e' }}>CV Maker Module</Text></View>;
 }
 const styles = StyleSheet.create({ center: { flex: 1, justifyContent: 'center', alignItems: 'center' } });
